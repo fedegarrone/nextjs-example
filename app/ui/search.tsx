9e8fde2fcdb0e0bd4b6145e00642959c89697223
fedegarrone <garronefederico97@gmail.com>
2024-02-07 09:44:28 -0300

    add pagination to invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,21 +5,22 @@ import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const { replace } = useRouter()
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log('Searching... ',term)
-    
-    const params = new URLSearchParams(searchParams)
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
     if (term) {
-      params.set('query',term)
+      params.set('query', term);
     } else {
-      params.delete('query')
+      params.delete('query');
     }
-    replace(`${pathname}?${params.toString()}`)
-  },300);
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
